ccad3338cc835756661295b508d42c6f2cbe9b6d
Nando Pena <fernando.pena@rackspace.com>
2017-04-29 19:04:33 -0500

    add documentation building; update dir structure

---
+++ src/index.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const _ = require('lodash')
+const Koa = require('koa')
+const Names = require('./names')
+const Questions = new (require('./questions'))()
+
+let app = new Koa()
+let router = new (require('koa-trie-router'))()
+
+// Log call and time
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// Pretty print option
+app.use(async (ctx, next) => {
+  await next();
+  if (ctx.query.pretty === '1') {
+      ctx.body = JSON.stringify(ctx.body, null, 4)
+  }
+});
+
+router.get('/admin/save-questions',
+    async function (ctx, next) {
+        Questions.save()
+        ctx.body = 'Questions saved'
+    }
+)
+
+router.get('/admin/info',
+    async function  (ctx, next) {
+        ctx.body = Questions.info()
+    }
+)
+
+router.get('/question',
+    async function (ctx, next) {
+        ctx.body = Questions.get()
+    }
+)
+
+router.get('/login',
+    async function (ctx, next) {
+        ctx.body = `Hello ${ Names.generate() }`
+    }
+)
+
+
+app.use(router.middleware())
+app.listen(3000)
---
+++ src/utils.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const fs = require('fs')
+
+/** Class containing utility methods to ease common tasks. */
+class Utils {
+    constructor () {
+    }
+
+    /**
+     * Asynchronously read UTF-8 encoded file using Promise pattern.
+     * 
+     * @param {string} file - The path string of file to read
+     * @return {Promise.<{}>}
+     *   - @resolve returns string file data<br/>
+     *   - @reject returns readFile error
+     */
+    static readFile (file) {
+        return new Promise(function(resolve, reject) {
+            fs.readFile(file, 'utf8', (err, data) => {
+                if (err) {
+                    reject(err)
+                }
+
+                resolve(data)
+            })
+        });
+    }
+
+    /**
+     * Asynchronously read UTF-8 encoded file as JSON using Promise pattern.
+     * 
+     * @param {string} file - The path string of file to read
+     * @return {Promise.<{}>}
+     *   - @resolve returns file data as JSON object<br/>
+     *   - @reject returns readFile error
+     */
+    static readJson (file) {
+        return Utils.readFile(file).then(data => JSON.parse(data))
+    }
+
+    /**
+     * Synchronously read UTF-8 encoded file as JSON.
+     * 
+     * @param {string} file - The path string of file to read
+     * @return {Object} file data as JSON object
+     */
+    static readJsonSync (file) {
+        let fileExists = fs.existsSync(file)
+        if (!fileExists) {
+            return null
+        }
+            
+        try {
+            return JSON.parse(fs.readFileSync(file))
+        } catch (error) {
+            return null
+        }
+    }
+
+}
+
+module.exports = Utils
